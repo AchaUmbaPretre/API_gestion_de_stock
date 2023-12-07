f49d21c1397cbfdbe849004a55538193e708c2d8
AchaUmbaPretre <achandambi@gmail.com>
2023-12-07 09:46:36 +0100

    commit

--- controllers/rapportControllers.js
+++ controllers/rapportControllers.js
@@ -4,3 +4,12 @@ const dotenv = require('dotenv');
 const moment = require('moment');
 
 dotenv.config();
+
+exports.getRapport = (req, res) => {
+    const q = "SELECT * FROM clients WHERE est_supprime = 0";
+     
+    db.query(q, (error, data) => {
+        if (error) res.status(500).send(error);
+        return res.status(200).json(data);
+    });
+}
