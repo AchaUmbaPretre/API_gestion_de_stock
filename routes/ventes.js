8adbfb6c281350d063d5f17031ec95737195baf7
AchaUmbaPretre <achandambi@gmail.com>
2023-12-07 10:12:38 +0100

    commit

--- routes/ventes.js
+++ routes/ventes.js
@@ -1,8 +1,11 @@
-const express = require('express')
+const express = require('express');
+const { getVente, postVente, deleteVente, putVente } = require('../controllers/venteControllers');
 
 const router = express.Router()
 
-router.get('/vente',)
-router.post('/vente',)
+router.get('/vente', getVente)
+router.post('/vente',postVente)
+router.put('/venteDelete/:id',deleteVente)
+router.put('/vente/:id',putVente)
 
 module.exports = router;
