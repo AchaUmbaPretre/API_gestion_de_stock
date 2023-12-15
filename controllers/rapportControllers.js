443f730a84b9e3f607914bdc669a13c7042137ec
AchaUmbaPretre <achandambi@gmail.com>
2023-12-15 15:46:08 +0100

    commit

--- controllers/rapportControllers.js
+++ controllers/rapportControllers.js
@@ -21,3 +21,22 @@ exports.getRapportVente = (req, res) => {
         return res.status(200).json(data);
     });
 }
+
+exports.getRapportAchats = (req, res) => {
+
+    const q = `SELECT p.id, p.nom_produit AS nom_produit, SUM(a.quantite) AS quantite_totale_achetee, 
+                    SUM(a.quantite * a.prix_unitaire) AS montant_total_achat, 
+                    c.quantite_stock AS quantite_en_stock
+                FROM vente a
+                JOIN produits p ON a.produit_id = p.id
+                JOIN chaussures c ON a.produit_id = c.produit_id
+                GROUP BY p.id, p.nom_produit, c.quantite_stock`;
+     
+    db.query(q, (error, data) => {
+        if (error) res.status(500).send(error);
+        return res.status(200).json(data);
+    });
+}
+
+
+
