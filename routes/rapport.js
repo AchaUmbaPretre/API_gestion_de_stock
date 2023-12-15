8db29c26819dee8cec202fd42dd6c994a217c22f
AchaUmbaPretre <achandambi@gmail.com>
2023-12-15 15:46:55 +0100

    commit

--- routes/rapport.js
+++ routes/rapport.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const { getRapport, getRapportVente } = require('../controllers/rapportControllers');
+const { getRapport, getRapportVente, getRapportAchats } = require('../controllers/rapportControllers');
 const router = express.Router();
 
 router.get('/', getRapportVente);
+router.get('/rapportAcha', getRapportAchats);
 
 module.exports = router;
